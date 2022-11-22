ffb1cf9445d18188cddaf42b8bc51bd34ae0ff6f
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2022-11-22 23:33:09 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
-grant all privileges on database db-bmstu-3 to test;
+grant all privileges on database "db-bmstu-3" to test;
 grant select, update, insert on assignments to test;
 grant select, update(function) on employees to test;
 grant select on projects to test;
