920d23d4c9b4f540402e9d05540f86978a940051
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2022-11-22 23:23:55 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
 grant all privileges on database demo to test;
 grant select, update, insert on bookings.aircrafts_data to test;
 grant select, update(airport_name) on bookings.airports_data to test;
-grant select on booking.flights to test;
+grant select on bookings.flights to test;
